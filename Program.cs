f1b9d652d02f7dac430b66bd57cf9f1153630835
canvery <164883012+canvery@users.noreply.github.com>
2024-04-02 16:19:20 +0800

    Changed int to double and created a menu
    Changed int value to double value and created a menu with options.

--- Program.cs
+++ Program.cs
@@ -1,28 +1,85 @@
-﻿namespace Project3
+﻿using System.Reflection.PortableExecutable;
+
+namespace Project3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("");
+            //Console.ReadLine();
+            MainMenu();
+        }
+        static void MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("1. Seconds to Minutes");
+            Console.WriteLine("2. Minutes to Hours");
+            Console.WriteLine("3. Hours to Days");
+            Console.WriteLine("4. Days to Months\n");
+            Console.Write("Please type what you would like to convert: ");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    SectoMin();
+                    break;
+                case "2":
+                    MintoHr();
+                    break;
+                case "3":
+                    HtoD();
+                    break;
+                case "4":
+                    DtoM();
+                    break;
+            }
+
+
+            MainMenu();
+        }
+        static void SectoMin()
+        {
+            Console.WriteLine("");
             Console.WriteLine("Seconds to Minutes");
             Console.Write("Seconds = ");
-            int seconds = Convert.ToInt32(Console.ReadLine());
+            double seconds = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Minutes = {0}\n", seconds / 60);
-
+            Console.WriteLine("");
+            Console.Write("Press Enter to return");
+            Console.ReadLine();
+        }
+        static void MintoHr()
+        {
+            Console.WriteLine("");
             Console.WriteLine("Minutes to Hours");
             Console.Write("Minutes = ");
-            int minutes = Convert.ToInt32(Console.ReadLine());
+            double minutes = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Hours = {0}\n", minutes / 60);
-
+            Console.WriteLine("");
+            Console.Write("Press Enter to return");
+            Console.ReadLine();
+        }
+        static void HtoD()
+        {
+            Console.WriteLine("");
             Console.WriteLine("Hours to days");
             Console.Write("Hours = ");
-            int hours = Convert.ToInt32(Console.ReadLine());
+            double hours = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Days = {0}\n", hours / 24);
-
+            Console.WriteLine("");
+            Console.Write("Press Enter to return");
+            Console.ReadLine();
+        }
+        static void DtoM()
+        {
+            Console.WriteLine("");
             Console.WriteLine("Days to Months");
             Console.Write("Days = ");
-            int days = Convert.ToInt32(Console.ReadLine());
+            double days = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Months = {0}", days / 30.44);
+            Console.WriteLine("");
+            Console.Write("Press Enter to return");
+            Console.ReadLine();
         }
     }
 }
